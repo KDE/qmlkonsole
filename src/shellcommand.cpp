ef1b8a357b4752d1a2af875b3b8e5105d7a40d0a
Jonah Brüchert <jbb@kaidan.im>
2023-02-17 23:07:11 +0100

    Use host-spawn

--- src/shellcommand.cpp
+++ src/shellcommand.cpp
@@ -1,10 +1,15 @@
 // SPDX-FileCopyrightText: 2022 Jonah Brüchert <jbb@kaidan.im>
+// SPDX-FileCopyrightText: 2022 Harald Sitter <sitter@kde.org>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "shellcommand.h"
 
 #include <KSandbox>
 #include <KShell>
+
+#include <pwd.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 ShellCommand::ShellCommand(QObject *parent)
     : QObject(parent)
@@ -25,7 +30,23 @@ QStringList ShellCommand::args() const
 QString ShellCommand::command() const
 {
     if (KSandbox::isFlatpak()) {
-        return QStringLiteral(R"(bash -c "flatpak-spawn --env=TERM=${TERM} --host sh -c 'exec $SHELL'")");
+        auto shell = []() {
+            // From Konsole: https://github.com/KDE/konsole/blob/c450e754c789915b6dd27514a18f311ba9249981/src/profile/Profile.cpp#L177
+            auto pw = getpwuid(getuid());
+            // pw: Do not pass the returned pointer to free.
+            if (pw != nullptr) {
+                QProcess proc;
+                proc.setProgram(QStringLiteral("getent"));
+                proc.setArguments({QStringLiteral("passwd"), QString::number(pw->pw_uid)});
+                KSandbox::startHostProcess(proc);
+                proc.waitForFinished();
+                return QString::fromUtf8(proc.readAllStandardOutput().simplified().split(':').at(6));
+            } else {
+                return QStringLiteral("bash");
+            }
+        }();
+
+        return QStringLiteral("host-spawn %1").arg(shell);
     } else {
         return qEnvironmentVariable("SHELL");
     }
